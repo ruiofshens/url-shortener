2edaaac1fcb66d8d2a79c8393112ea61c0ecaf80
Shen Rui <50244514+ruiofshens@users.noreply.github.com>
2022-09-23 06:03:10 +0800

    feat: create frontend interface (#2)
    * feat: add frontend interface
    
    * chore: update CHANGELOG.md, cleanups

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { shortenURL } from './services/api';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import URLShortenerPage from './pages/UrlShortenerPage';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
-
-  const sample = "sample URL"
-
-  const handleClick = () => {
-    shortenURL(sample)
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained" onClick={handleClick}>Hello World</Button>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      <URLShortenerPage/>
+    </ThemeProvider>
   );
 }
 
